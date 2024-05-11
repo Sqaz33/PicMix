9380bda4f049c073928c5cc50d40927001fba018
Matveev Stepan Andreevich <mset321@gmail.com>
2024-05-11 14:00:52 +0300

    Закончил с методами Pic

--- pic.py
+++ pic.py
@@ -1,12 +1,29 @@
 import copy
+from typing import Any
 
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
 import skimage
 
 
 class Pic:
+    """
+    Class for a picture.
+
+    Attributes:
+        pic (np.array): The picture presented as a numpy array.
+        color (str): The color of the monochrome picture.
+        is_monochrome (bool): Whether the picture is monochrome.
+        height (int): The height of the picture.
+        width (int): The width of the picture.
+    """
     def __init__(self, pic: np.array, color="Greys"):
+        """
+        Create the new Pic object.
+        :param pic (np.array): Picture as a NumPy array.
+        :param color (str): Monochrome picture color as a string.
+        """
         if len(pic.shape) not in (2, 3):
             raise "required color model RGB or monochromatic with 8 bits per channel"
 
@@ -57,18 +74,22 @@ class Pic:
         new_rows = np.zeros(size, dtype=self._pic.dtype)
         self._pic = np.hstack([self._pic, new_rows])
 
-    #TODO: подумать над названием (получение среднего пикселя из диапазона)
-    def get_average_pixel_from_range(self, ):
-        pass
+    def get_average_pixel_from_rectangle(self, top_left_corner: (int, int), bottom_right_corner: (int, int)) -> Any:
+        el_slice = 1 if self._is_monochrome else self._pic.shape[-1]
+        sub_array = self._pic[
+                    top_left_corner[0]:bottom_right_corner[0] + 1,
+                    top_left_corner[1]:bottom_right_corner[1] + 1,
+                    0:el_slice
+                    ]
+        sm = np.sum(np.sum(sub_array, axis=0), axis=0)
+        average = sm / ((bottom_right_corner[0]+1) * (bottom_right_corner[1]+1))
+        return np.ceil(average).astype(self._pic.dtype)
 
-    #TODO: переделать вычисление среднего пикселя
-    def get_average_pixel(self):
-        sm = 0 if self._is_monochrome else np.zeros((1, 3), dtype=self._pic.dtype)
-        for row in self._pic:
-            for col in row:
-                for p in col:
-                    sm += p
-        return sm / (self._width * self._height)
+    def get_average_pixel(self) -> Any:
+        return self.get_average_pixel_from_rectangle(
+            (0, 0),
+            (self._height-1, self._width-1)
+        )
 
     def display_pic_pyplot(self):
         if self._is_monochrome:
@@ -89,7 +110,15 @@ def load_pics(paths_to_pictures) -> list[Pic]:
     return pics
 
 
+import unittest
+
+
+class TestPic(unittest.TestCase):
+    def test_test(self):
+        snipers = skimage.io.imread("photo_2024-05-10_10-18-08.jpg")
+        snipers_pic = Pic(snipers)
+        print(snipers_pic.get_average_pixel())
+
+
 if __name__ == "__main__":
-    snipers = skimage.io.imread("photo_2024-05-10_10-18-08.jpg")
-    snipers_pic = Pic(snipers)
-    print(snipers_pic.get_average_pixel())
+    unittest.main()
